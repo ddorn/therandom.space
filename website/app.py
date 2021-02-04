f009b573dfd01bc01b934532ff59ea5a48f8e5c9
ddorn <diego.dorn@free.fr>
2021-02-04 19:53:32 +0100

    add other generators

--- website/app.py
+++ website/app.py
@@ -39,14 +39,39 @@ dictConfig(
 app = Flask(__name__)
 
 
+def word_page(proc_path, title, min_len=1):
+    occ = load_preproc(open(proc_path))
+    w = ""
+    while len(w) < min_len:
+        w = gen(occ)
+    return render_template("word.html", bled=w, title=title)
+
 @app.route("/")
 def home():
     occ = load_preproc(open("data/alsa.proc2"))
     w = ""
     while len(w) < 5:
         w = gen(occ)
-    return render_template("home.html", bled=w, title="Alsace")
+    return render_template("word.html", bled=w, title="Alsace")
 
+
+@app.route("/proverb")
+def proverb():
+    return word_page("data/citations.proc2", "Proverbe")
+
+
+@app.route("/french-word")
+def french_word ():
+    return word_page("data/fr.proc2", "French Word", 4)
+
+
+@app.route("/alsace")
+def alsace():
+    return word_page("data/alsa.proc2", "Alsace")
+
+@app.route("/name")
+def name():
+    return word_page("data/mec.proc2", "Name")
 
 @app.route("/about")
 def about():
