c2450e667935e4edc25c3da7a323eb91d1f2c128
ddorn <diego.dorn@free.fr>
2021-02-10 04:09:55 +0100

    tip stay away + animation

--- website/static/random.js
+++ website/static/random.js
@@ -36,11 +36,15 @@ function make_like_button(like_button, quote_elem, kind) {
 
 /// Text can be a string or a function returning one
 function make_tooltip(elem, text) {
+    let has_tip = false;
+
     function cancelTip() {
         document.querySelectorAll('.tooltip').forEach(tip => tip.remove())
     }
 
     function showTip() {
+        if (has_tip) return;
+
         var tip = document.createElement('div');
 
         tip.appendChild(document.createTextNode(text instanceof Function ? text() : text));
@@ -53,11 +57,14 @@ function make_tooltip(elem, text) {
         let top = elem_rect.top - tip_rect.height - 12 + 'px';
         tip.style.left = elem_rect.left + elem_rect.width / 2 - tip_rect.width / 2 + 'px'
         tip.style.top = top;
+
+        has_tip = true;
     }
 
     elem.addEventListener('click', cancelTip)
     elem.addEventListener('mouseover', showTip)
-    elem.addEventListener('mouseout', cancelTip)
+    elem.addEventListener('mouseleave', cancelTip)
+    elem.addEventListener('mouseleave', _ => has_tip = false)
 }
 
 function copy(text) {
